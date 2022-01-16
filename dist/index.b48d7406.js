4a57c5955a415e52b96053e2b1111bcfd1efc805
david361215 <1138890119@qq.com>
2022-01-16 15:50:39 +0800

    init

---
+++ dist/index.b48d7406.js
@@ -0,0 +1,2 @@
+var myChart=echarts.init(document.getElementById("skills")),option={radar:{indicator:[{name:"静态页面",max:100},{name:"编程基础",max:100},{name:"Vue",max:100},{name:"React",max:100},{name:"项目开发",max:100},{name:"沟通能力",max:100}]},series:[{name:"Budget vs spending",type:"radar",data:[{value:[100,70,90,85,85,70],name:"能力雷达"}]}]};myChart.setOption(option);
+//# sourceMappingURL=index.b48d7406.js.map
